00385eb0500adf06ffb3e449c74ce19624722bd0
Justin Heideman <jheideman@mpr.org>
2013-08-30 14:40:31 -0500

    adding notifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,6 +122,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-notify');
 
     grunt.registerTask('deploy', ['jshint', 'sass:deploy', 'requirejs', 'modernizr', 'uglify:requirejs', 'concat:deploy']);
 
