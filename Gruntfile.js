a8d40e74dc5a95b0244545bbe86727d52ac874fd
Justin Heideman <jheideman@mpr.org>
2013-09-05 14:27:09 -0500

    working on switching to less

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
 
     // Project configuration.
