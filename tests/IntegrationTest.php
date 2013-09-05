45d06b0b41369605b53545d46b5dbc4b45351229
Paul Wenzel <paul@pwenzel.info>
2013-09-05 15:10:39 -0500

    use goutte/guzzle for function tests on the front-end

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -1,10 +1,21 @@
 <?php
 
+use Goutte\Client;
+
 class IntegrationTest extends PHPUnit_Framework_TestCase
+
 {
+
     public function testSlimLoaded()
     {
         $this->assertTrue(class_exists('\Slim\Slim'), "Slim Framework should exist.");
+    }
+
+    public function testIndex()
+    {
+    	$client = new Client();
+	    $crawler = $client->request('GET', 'http://require.dev/');
+		$this->assertEquals('Require js demo project', $crawler->filter('title')->text());
     }
 
 }
