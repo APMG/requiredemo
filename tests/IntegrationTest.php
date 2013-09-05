ef61197444bda99b7957bf71463bef32b6841df6
Paul Wenzel <paul@pwenzel.info>
2013-09-05 15:54:54 -0500

    functional tests with SimpleHTMLDOM

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -1,21 +1,31 @@
 <?php
 
-use Goutte\Client;
+/**
+ * Functional tests use SimpleHTMLDOM
+ * @link( SimpleHTMLDOM Manual, http://simplehtmldom.sourceforge.net/manual.htm)
+ */
+
+use Sunra\PhpSimple\HtmlDomParser;
 
 class IntegrationTest extends PHPUnit_Framework_TestCase
 
 {
 
-    public function testSlimLoaded()
+    public function testSlimExists()
     {
         $this->assertTrue(class_exists('\Slim\Slim'), "Slim Framework should exist.");
     }
 
     public function testIndex()
     {
-    	$client = new Client();
-	    $crawler = $client->request('GET', 'http://require.dev/');
-		$this->assertEquals('Require js demo project', $crawler->filter('title')->text());
+
+        $html = HtmlDomParser::file_get_html( 'http://require.dev/' );
+        $e = $html->find("body", 0);
+        $this->assertContains("This is the home page", $e->plaintext);
+
+        $title = $html->find('title');
+        $this->assertContains("Require js demo project", $title[0]->plaintext);
+
     }
 
 }
