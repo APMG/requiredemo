d9b7d2a071508980ae10126bd8cdc2b9c05b58c3
Justin Heideman <jheideman@mpr.org>
2013-09-09 10:59:09 -0500

    organizing files

--- public_html/js/app/mediaplayer.js
+++ public_html/js/app/mediaplayer.js
@@ -1,33 +1,6 @@
-define(["jquery", 'jquery-pjax'], function ($) {
-    // technically we might not need to load jquery-pjax here if we don't have pushState/history
-    // but, we care less about performance in IE/desktop, so...
+define(["jquery"], function ($) {
     'use strict';
-
-
-    function pjaxScanLinks(container){
-        console.log(container);
-        $(container).find('a').each(function(){
-            if (this.host === window.location.host){
-                $(this).attr('data-pjax','true');
-                console.log(this);
-            }
-        });
-    }
-
     
-    // Only load pjax if html5 pushState (history) is available in this browser 
-    if (Modernizr.history){
-
-        pjaxScanLinks('#pjax-container');
-
-        $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container');
-        // We need to check the incoming data for any links to the site itself
-        // For now, this is just the host, but in the future it will need to look at URL patterns
-        $('#pjax-container').on('pjax:end', function(event){
-            pjaxScanLinks(event.target);
-        });
-    }
-
     //fake load media player
     $('#mediaPlayer').on('click', function (e) {
         var self = this;
--- public_html/js/init.js
+++ public_html/js/init.js
@@ -6,25 +6,26 @@
 "use strict";
 
 requirejs.config({
-		"baseUrl": "/js/",
-		"paths": {
-			"app": "app",
-			"jquery": "../vendor/jquery/jquery",
-			"jquery-pjax": "../vendor/jquery-pjax/jquery.pjax",
-			'soundmanager2': '//common.publicradio.org/media_player/1.2.2/script/lib/soundmanager2-jsmin',
-			'apmplayer': '//common.publicradio.org/media_player/1.2.2/script/apmplayer-all.min'
-		},
-		"shim": {
-				"jquery-pjax": ["jquery"],
-				'soundmanager2': {
-						exports: 'soundManager'
-				},
-				'apmplayer': {
-					exports: 'apmplayer_ui',
-					deps: ["jquery", 'soundmanager2']
-				}
-		}
+        "baseUrl": "/js/",
+        "paths": {
+            "app": "app",
+            "jquery": "../vendor/jquery/jquery",
+            "jquery-pjax": "../vendor/jquery-pjax/jquery.pjax",
+            'soundmanager2': '//common.publicradio.org/media_player/1.2.2/script/lib/soundmanager2-jsmin',
+            'apmplayer': '//common.publicradio.org/media_player/1.2.2/script/apmplayer-all.min'
+        },
+        "shim": {
+                "jquery-pjax": ["jquery"],
+                'soundmanager2': {
+                        exports: 'soundManager'
+                },
+                'apmplayer': {
+                    exports: 'apmplayer_ui',
+                    deps: ["jquery", 'soundmanager2']
+                }
+        }
 });
 
 // Load the main app module to start the app
-requirejs(["app/main"]);
+requirejs(["app/pjax"]);            // handles links
+requirejs(["app/mediaplayer"]);     // handles media player
