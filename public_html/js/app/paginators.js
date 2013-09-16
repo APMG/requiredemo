812f202445a206ed3da6dcd6a34a3f1a70beb626
Justin Heideman <jheideman@mpr.org>
2013-09-16 09:53:52 -0500

    updated paginators to work better after pjax

--- public_html/js/app/paginators.js
+++ public_html/js/app/paginators.js
@@ -5,24 +5,38 @@ define(['jquery','scrollMonitor' ], function ($, scrollMonitor) {
      * paginators line up with the box on the page that should contain them 
      */
     //$(document).ready(function(){
-        var $element = $('#paginators');
+    var $element = $('#paginators');
 
-        console.log($element, scrollMonitor.viewportHeight/2);
-        var watcher = scrollMonitor.create( $element, scrollMonitor.viewportHeight/2);
-        console.log(watcher);
-
-        // TODO: This is currently broken in IE 10
-        // Fix it.
-        watcher.fullyEnterViewport(function() {
-            // somewhat redundant, but we need to re-query the element every time
-            // it can get out of whack by pjax
-            $element = $('#paginators');
-            // The contents of this function should only fire on medium and larger screens 
-            if ( $("#checkSmall").is(':hidden')  ){
-                console.log('stateChange!');
-                console.log(this);
-                $element.find('a').addClass('notfixed').css('top',$element.position().top);
-            }
+    // If there are no paginators, we add an event to pjax:complete to try adding watchPaginators later
+    // This way if we come to a page w/o paginators, they will work correctly after pjax
+    if ($element.length === 0){
+        $(document).on('pjax:complete', function(){
+            watchPaginators();
         });
-    //});
+    } else {
+        watchPaginators();
+    }
+
+    function watchPaginators(){
+        var $element = $('#paginators');
+        if ($element.length > 0){
+            console.log($element, scrollMonitor.viewportHeight/2);
+            var watcher = scrollMonitor.create( $element, scrollMonitor.viewportHeight/2);
+            console.log(watcher);
+
+            // TODO: This is currently broken in IE 10
+            // Fix it.
+            watcher.fullyEnterViewport(function() {
+                // somewhat redundant, but we need to re-query the element every time
+                // it can get out of whack by pjax
+                $element = $('#paginators');
+                // The contents of this function should only fire on medium and larger screens 
+                if ( $("#checkSmall").is(':hidden')  ){
+                    console.log('stateChange!');
+                    console.log(this);
+                    $element.find('a').addClass('notfixed').css('top',$element.position().top);
+                }
+            });
+        }
+    }
 });
